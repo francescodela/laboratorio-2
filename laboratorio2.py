dff32f77aaa932743044862c23da1af1f22360e8
francescodela <franklin.hernandez@correounivalle.edu.co>
2024-08-21 11:25:43 -0500

    Update laboratorio2.py

--- laboratorio2.py
+++ laboratorio2.py
@@ -3,8 +3,8 @@ class Mascota  :
         self.edad = edad 
         self.color = color 
         self.tipo = tipo 
-matias = Mascota (5, "blanco", "perro")
-zara = Mascota (2, "naranja", "gato" )
+matias = Mascota ("5 años", "blanco", "perro")
+zara = Mascota ("2 años", "naranja", "gato" )
  
 print (type (matias))
 print (type (zara))
